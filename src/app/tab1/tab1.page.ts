417eaeef51a89f1770b7b20d188eda0fa5a79537
john <jxn5229@rit.edu>
2022-05-11 02:45:22 -0400

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  posts = [];
+  segment = 'home';
+  opts = {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesOffsetBefore: 0
+  };
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.http.get('https://devdactic.fra1.digitaloceanspaces.com/twitter-ui/tweets.json').subscribe((data: any) => {
+      console.log('tweets: ', data.tweets);
+      this.posts = data.tweets;
+    });
+  }
 }
